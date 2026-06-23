4f7c9a1678a5741c80c33581c2a39c316df74211
codexraphel77 <moolchandanipiyush912@gmail.com>
2026-06-23 12:52:49 +0530

    updated(23-06-2026)

--- intprep(2).py
+++ intprep(2).py
@@ -117,6 +117,146 @@ a separate group to avoid index errors."'''
 # print(l2)
 
 
+'''51.	Find element with second least frequency '''
+# l=[1,2,2,3,3,3,4,4,4,4]
+# min_freq=9
+# se_min_freq=9
+# for i in l:
+#     count=0
+#     for j in l:
+#         if i==j:
+#             count+=1
+#     if count<min_freq:
+#        se_min_freq=min_freq
+#        min_freq=count
+#     elif count<se_min_freq and count!=min_freq:
+#         se_min_freq=count
+#         se_element=i
+# print(se_element)
+# optimized
+# l=[1,2,2,3,3,3,4,4,4,4]
+# d={}
+# for i in l:
+#     d[i]=d.get(i,0)+1
+# freq=sorted(set(d.values()))
+# second_least=freq[1]
+# for k,v in d.items():
+#     if v==second_least:
+#         print(k)
+    
+'''52.	Find all elements appearing odd number of times '''
+# l=[1,2,2,3,3,3,4,4,4,4]
+# l2=[]
+# for i in l:
+#     freq=0
+#     for j in l:
+#         if i==j:
+#             freq+=1
+#     if freq%2!=0 and i not in l2:
+#         l2.append(i)
+# print(l2)
+
+'''53.	Remove first repeating element only '''
+# l=[1,2,2,3,3,3,4,4,4,4] 
+# for i in l:
+#     freq=0
+#     for j in l:
+#         if i==j:
+#             freq+=1
+#     if freq>1:
+#         l.remove(i)
+#         break
+# print(l)
+
+'''Remove all occurrences of the first repeating element'''
+# l=[1,2,2,3,3,3,4,4,4,4] 
+# repeat=None
+# for i in l:
+#     freq=0
+#     for j in l:
+#         if i==j:
+#             freq+=1
+#     if freq>1:
+#         repeat=i
+#         break
+# while repeat in l:
+#     l.remove(repeat)
+# print(l)
+
+'''54.	Remove last repeating element only '''
+# l=[1,2,2,3,3,3,4,4,4,4] 
+# for i in range(len(l)-1,-1,-1):
+#     freq=0
+#     for j in l:
+#         if l[i]==j:
+#             freq+=1
+#     if freq>1:
+#         l.remove(l[i])
+#         break
+# print(l)
+
+'''Remove all occurrences of the last repeating element'''
+# l=[1,2,2,3,3,3,4,4,4,4] 
+# repeat=None
+# for i in range(len(l)-1,-1,-1):
+#     freq=0
+#     for j in l:
+#         if l[i]==j:
+#             freq+=1
+#     if freq>1:
+#         repeat=l[i]
+#         break
+# while repeat in l:
+#     l.remove(repeat)
+# print(l)
+
+
+'''55.	Find first element whose frequency > average frequency '''
+# l=[1,2,2,3,3,3,4,4,4,4]
+# freq=[]
+# for i in l:
+#     count=0
+#     for j in l:
+#         if i==j:
+#             count+=1
+#     if count not in freq:
+#         freq.append(count)
+# sum=0
+# for i in freq:
+#     sum+=i
+# avg=sum/len(freq)
+# for i in l:
+#     count=0
+#     for j in l:
+#         if i==j:
+#             count+=1
+#     if count>avg:
+#         print(i)
+#         break
+
+# optimized
+# l=[1,2,2,3,3,3,4,4,4,4]
+# d={}
+# for i in l:
+#     d[i]=d.get(i,0)+1
+# total=0
+# for v in d.values():
+#     total+=v
+# avg=total/len(d)
+# for k,v in d.items():
+#     if v>avg:
+#         print(k)
+#         break
+
+''''''
+
+
+  
+
+
+        
+
+
 
         
 
