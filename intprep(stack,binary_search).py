047c04c7ae88f5a3c0431919b1933b36ec9e89f4
codexraphel77 <moolchandanipiyush912@gmail.com>
2026-08-06 13:41:09 +0530

    updated dsa (06-08-2026)

--- intprep(stack,binary_search).py
+++ intprep(stack,binary_search).py
@@ -1,3 +1,5 @@
+# -----------------------------------------STACK-----------------------------------------------------
+'''valid parenthesis'''
 # s = "()]{}"
 # stack =[]
 # is_valid = True
@@ -72,5 +74,93 @@
 #     stack.append(i)
 # print(ans)
 
-    
+
+'''•	Stock Span '''
+# l = [100, 80, 60, 70, 60, 75, 85]
+# stack=[]
+# ans=[]
+# for i in range(len(l)):
+#     while stack and l[stack[-1]]<=l[i]:
+#         stack.pop()
+#     if not stack:
+#         ans.append(i+1)
+#     else:
+#         ans.append(i-stack[-1])
+#     stack.append(i)
+# print(ans)
+
+
+'''•	Remove Adjacent Duplicates '''
+# s = "abbaca"
+# stack = []
+# for i in s:
+#     if stack and stack[-1]==i:
+#         stack.pop()
+#     else:
+#         stack.append(i)
+# result = "".join(stack)
+# print(result)
+
+# ---------------------------------------Binary Search------------------------------------------------
+
+'''•	Binary Search ''' 
+# l = [2, 4, 7, 10, 13, 18, 21]
+# target = 13
+# left = 0
+# right = len(l)-1
+# found = False
+# while left<=right:
+#     mid = (left+right)//2
+#     if l[mid]==target:
+#         print(mid)
+#         found = True
+#         break
+#     elif l[mid]<target:
+#         left = mid+1
+#     else:
+#         right = mid-1
+# if not found:
+#     print(-1)
+
+# fuction approach
+# def binary_search(l, target):
+#     left = 0
+#     right = len(l) - 1
+#     while left <= right:
+#         mid = (left + right) // 2
+#         if l[mid] == target:
+#             return mid
+#         elif l[mid] < target:
+#             left = mid + 1
+#         else:
+#             right = mid - 1
+#     return -1
+# x = [2, 4, 7, 10, 13, 18, 21]
+# t = 13
+# print(binary_search(x,t))
+
+
+'''•	First Occurrence '''
+# def first_occurence(l,target):
+#     left = 0
+#     right = len(l)-1
+#     ans = -1
+#     while left<=right:
+#         mid = (left+right)//2
+#         if l[mid] == target:
+#             ans = mid
+#             right = mid -1
+#         elif l[mid]<target:
+#             left = mid +1
+#         else:
+#             right = mid -1
+#     return ans
+# x = [2, 4, 4, 4, 7, 10]
+# t = 4
+# print(first_occurence(x,t))
+
+
+
+
+
 
