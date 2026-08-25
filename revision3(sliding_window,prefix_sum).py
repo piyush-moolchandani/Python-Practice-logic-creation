7a11338cf532580b2ed8178ffabe0fc9037b3302
codexraphel77 <moolchandanipiyush912@gmail.com>
2026-08-25 13:27:44 +0530

    updated dsa revision(25-08-2026)

--- revision3(sliding_window,prefix_sum).py
+++ revision3(sliding_window,prefix_sum).py
@@ -108,3 +108,152 @@
 # print(l[start:start+k])
 # print(max_sum)
 
+
+'''•	Maximum Consecutive Ones '''
+# l = [1, 1, 0, 1, 1, 1]
+# count=0
+# max_count=0
+# for i in l:
+#     if i==1:
+#         count+=1
+#         if count>max_count:
+#             max_count=count
+#     else:
+#         count=0
+# print(max_count)
+
+'''•	•	Maximum Consecutive Ones III '''
+# l = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0]
+# k = 2
+# left=0
+# zero_count=0
+# max_len=0
+# for right in range(len(l)):
+#     if l[right]==0:
+#         zero_count+=1
+#     while zero_count>k:
+#         if l[left]==0:
+#             zero_count-=1
+#         left+=1
+#     curren_len=right-left+1
+#     if curren_len>max_len:
+#         max_len=curren_len
+# print(max_len)
+
+
+'''•	Longest Substring Without Repeating Characters '''
+
+# s = "abcabcbb"
+# left=0
+# max_len=0
+# seen=set()
+# for right in range(len(s)):
+#     while s[right] in seen:
+#         seen.remove(s[left])
+#         left+=1
+#     seen.add(s[right])
+#     curren_len=right-left+1
+#     if curren_len>max_len:
+#         max_len=curren_len
+# print(max_len)
+
+
+'''•	Fruits Into Basket '''
+# fruits = [1, 2, 1, 2, 3]
+# left=0
+# max_len=0
+# d={}
+# start=0
+# for right in range(len(fruits)):
+#     if fruits[right] in d:
+#         d[fruits[right]] = d[fruits[right]] + 1
+#     else:
+#         d[fruits[right]] = 1
+#     while len(d)>2:
+#         d[fruits[left]]-=1
+#         if d[fruits[left]]==0:
+#             del d[fruits[left]]
+#         left+=1
+#     curren_len=right-left+1
+#     if curren_len>max_len:
+#         max_len=curren_len
+#         start=left
+# print(max_len)
+# print(fruits[start:start+max_len])
+
+'''max con rev2'''
+l = [1, 1, 0, 1, 1, 1]
+count=0
+max_len=0
+for i in l:
+    if i==1:
+        count+=1
+        if count>max_len:
+            max_len=count
+    else:
+        count=0
+print(max_len)
+
+
+'''max con 3 rev2'''
+l = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0]
+k = 2
+left=0
+max_len=0
+zero_count=0
+for right in range(len(l)):
+    if l[right]==0:
+        zero_count+=1
+    while zero_count>k:
+        if l[left]==0:
+            zero_count-=1
+        left+=1
+    curren_len=right-left+1
+    if curren_len>max_len:
+        max_len=curren_len
+print(max_len)
+
+
+'''long sub unique rev2'''
+s = "abcabcbb"
+left=0
+seen=set()
+max_len=0
+for right in range(len(s)):
+    while s[right] in seen:
+        seen.remove(s[left])
+        left+=1
+    seen.add(s[right])
+    curren_len=right-left+1
+    if curren_len>max_len:
+        max_len=curren_len
+print(max_len)
+
+
+'''fruits into basket rev2'''
+fruits = [1, 2, 1, 2, 3]
+left=0
+max_len=0
+d={}
+start=0
+for right in range(len(fruits)):
+    if fruits[right] in d:
+        d[fruits[right]]=d[fruits[right]]+1
+    else:
+        d[fruits[right]]=1
+    while len(d)>2:
+        d[fruits[left]]-=1
+        if d[fruits[left]]==0:
+            del d[fruits[left]]
+        left+=1
+    curren_len=right-left+1
+    if curren_len>max_len:
+        max_len=curren_len
+        start=left
+print(max_len)
+print(fruits[start:start+max_len])
+    
+
+
+
+
